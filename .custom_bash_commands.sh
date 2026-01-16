f40b9a8c692c715293790a3e5c363f5f314508f3
Lisette Scheer <lisette@bywatersolutions.com>
2026-01-16 10:33:10 -0800

    Updated custom commands
    This commit adds git complete to debranch after running newktd
    Additionally this adds notes and 2 new functions:
    
    listfunctions which lists all functions as long as theres a ## note
    readfunction, which lists all funtions, then promts for a function name and echos the full command

--- .custom_bash_commands.sh
+++ .custom_bash_commands.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-#Checkout a branch with the branch name and origin/main
-# branch branchname = git checkout -B branchname origin/main
+## branch | Checkout a branch with the branch name and origin/main
+#*    example: branch branchname = git checkout -B branchname origin/main
 function branch() {
     git checkout -B $1 origin/main
 }
 
-#delete branch 
-#stash with a message of unstaged branchname, checkout main, delete branchname, pull
+## debranch stash with a message of unstaged branchname, checkout main, delete branchname, pull
+#*    example: debranch bug_12345
+#*    note: can use git branch autocomplete if you've run the newktd command in this file 
 function debranch() {
-    git stash save -m unstaged $1
+    git stash save -m unstaged $1 changes
     git checkout main
     git branch -D $1 
     git pull
 }
 
-#after updates, run dbic, then update database, then restart all, then yarn build. Used when updating the branch or applying branches with changes to the database.
+## dbupdated | after updates, run dbic, then update database, then restart all, then yarn build. Used when updating the branch or applying branches with changes to the database.
 function dbupdates(){
     dbic
     updatedatabase
@@ -23,72 +24,111 @@ function dbupdates(){
     yarn build
 }
 
-#after updates restart_all and yarn build, used when updating patches with no db updates or after making changes that require the rebuilds.
+## applied | after updates restart_all and yarn build, used when updating patches with no db updates or after making changes that require the rebuilds.
 function applied(){
     restart_all
     yarn build
 }
 
-#turn on and add ssh key
+## setssh | turn on and add ssh key
 function setssh(){
     eval "$(ssh-agent -s)"
     ssh-add /kohadevbox/koha/shared/gitssh_id
 }
 
-#run after every new build of ktd/when opening a new window
-#sets my ssh, sources aliases, and builds the locate database.
+## newktd | sets my ssh, sources aliases, and builds the locate database.
+#*    note: run after every new build of ktd/when opening a new window
+
 function newktd(){
     setssh
     source shared/.bash_aliases
     sudo /etc/cron.daily/plocate
+    source /usr/share/bash-completion/completions/git
+    __git_complete debranch __git_main
 }
 
-#use perl-git-bz to attach
-#example: atch 1234 3
-# becomes /kohadevbox/perl-git-bz/bin/git-bz attach -e 1234 HEAD~3..
-#this opens the edit screen and for attaching the last 3 commits to bug 1234.
+## use perl-git-bz
+##  attach -e $1 HEAD~$2..
+#*    example: atch 1234 3 | becomes /kohadevbox/perl-git-bz/bin/git-bz attach -e 1234 HEAD~3..
+#*    note: this opens the edit screen and for attaching the last 3 commits to bug 1234.
 function atch(){
     /kohadevbox/perl-git-bz/bin/git-bz attach -e $1 HEAD~$2..
 }
 
+##  aply $1 |  apply bug 1234
+#*    example: aply 1234
+function aply(){
+    /kohadevbox/perl-git-bz/bin/git-bz apply $1
+}
+
+## hre | hard reset to origin main
 function hre(){
     git reset --hard origin/main
 }
 
-#search the git log
+## glg | search the git log
 function glg(){
     git log --grep='$1'
 }
 
-#find logs that changed a specific string
+## gls | find logs that changed a specific string
+#*    example: gls string 
 function gls(){
     git log -S $1
 }
 
-#git log between 2 commits
+## glc $1 $2 | git log between 2 commits
+
 function glc(){
     git log $1..$2
 }
 
-#git log only a number of commits
+## gln $1 | git log only a number of commits
 function gln(){
     git log -n $1
 }
-#git log only a number of commits with --oneline
+## glno $1 | git log only a number of commits with --oneline
 function glno(){
     git log -n $1 --oneline
 }
 
-#list full stash
+## Stash
+##  shashl | list full stash
 function stashl(){
     git stash list
 }
+##  stashs | save stash
 function stashs(){
     git stash save
 }
+
+##  stashm $1 | save with message
 function stashm(){
     git stash save -m $1
 }
+
+##  stasha $1 | apply a stash
 function stasha(){
     git stash apply $1
+}
+
+## ipl | install plugins and restart all
+function ipl (){
+    /kohadevbox/koha/misc/devel/install_plugins.pl
+    restart_all
+}
+
+## listfunctions | Search my functions
+function listfunctions() { grep -i "##"  /kohadevbox/koha/shared/.custom_bash_commands.sh; }
+
+## readfunction | more detail on a specific function
+readfunction(){
+  echo
+  grep -i "##" /kohadevbox/koha/shared/.custom_bash_commands.sh 
+  echo
+  echo -n "enter function name: "
+  read -e name
+  echo
+  clear
+  sed -n "/${name}/,/##/p" /kohadevbox/koha/shared/.custom_bash_commands.sh
 }
